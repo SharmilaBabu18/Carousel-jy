2afa6a43f58e397a87819582f5cd213fedc0a178
SharmilaBabu18 <119619491+SharmilaBabu18@users.noreply.github.com>
2023-12-16 18:42:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Carousel from './components/Carousel';
+
+function App() {
+  return (
+    <div className='App'>
+       <div className="Box">
+      <Carousel/>
+    </div>
+    </div>
+   
+  );
+}
+
+export default App;
